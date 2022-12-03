76189fa49aaae813310b52d13bc73ebe65c45126
Prashant tak <prashantt4333@gmail.com>
2022-12-03 17:24:02 +0530

    first js addition statment

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//Statment 1
+console.log('my name is prashant tak from pratapgarh ');
+
+var x = 4;
+var y = 20;
+
+//Display the output
+console.log('the Addition of ',x + y);
+window.alert(x + y)
+document.write(x + y)
+console.log('the Subtration of ',y - x);
+console.log('the Multiplation of ',x * y);
+console.log('the Division of ',x / y);
